eeeecaef487ee1797c83cb55a402547182a9145d
GustavoPelissari <gustavo12cristina@gmail.com>
2026-04-08 19:51:47 -0300

    Inserindo a logo da Seleção

--- app/login.tsx
+++ app/login.tsx
@@ -1,7 +1,7 @@
 import { Ionicons } from "@expo/vector-icons";
 import { useRouter } from "expo-router";
 import { useState } from "react";
-import { Alert, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Alert, KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function LoginScreen() {
@@ -12,7 +12,7 @@ export default function LoginScreen() {
     function trocarEstadoSenha() {
         if (secureText === true) {
             setSecureText(false);
-        }else {
+        } else {
             setSecureText(true);
         }
     }
@@ -27,49 +27,45 @@ export default function LoginScreen() {
             style={styles.container}
         >
             <View style={styles.innerContainer}>
-                <Ionicons 
-                    name="apps-outline" 
-                    size={64} 
-                    color={"#007AFF"}
-                    style={styles.logo} 
-                />
+                <Image source={require('../../img/logo.png')} style={styles.logo} />
                 <Text style={styles.title}>Acesse sua conta</Text>
-
-                <Text style={styles.label}>E-mail</Text>
-                <TextInput 
-                    style={styles.input}
-                    placeholder="email@example.com"
-                    keyboardType="email-address"
-                />
-
-                <Text style={styles.label}>Senha</Text>
-                <View style={styles.passwordContainer}>
-                    <TextInput 
-                        style={styles.passwordInput}
-                        placeholder="*********"
-                        secureTextEntry={secureText} 
-                    />
-                    <TouchableOpacity 
-                        onPress={trocarEstadoSenha}
-                        style={styles.iconContainer}
-                    >
-                        <Ionicons
-                            name={secureText ? "eye-off-outline" : "eye-outline"}
-                            size={20}
-                            color={"#8e8e93"}
-                        />
-                    </TouchableOpacity>
-                </View>
-
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={logar}
-                >
-                    <Text style={styles.buttonText}>Entrar</Text>
-                </TouchableOpacity>
-
             </View>
+
+            <Text style={styles.label}>E-mail</Text>
+            <TextInput
+                style={styles.input}
+                placeholder="email@example.com"
+                keyboardType="email-address"
+            />
+
+            <Text style={styles.label}>Senha</Text>
+            <View style={styles.passwordContainer}>
+                <TextInput
+                    style={styles.passwordInput}
+                    placeholder="*********"
+                    secureTextEntry={secureText}
+                />
+                <TouchableOpacity
+                    onPress={trocarEstadoSenha}
+                    style={styles.iconContainer}
+                >
+                    <Ionicons
+                        name={secureText ? "eye-off-outline" : "eye-outline"}
+                        size={20}
+                        color={"#8e8e93"}
+                    />
+                </TouchableOpacity>
+            </View>
+
+            <TouchableOpacity
+                style={styles.button}
+                onPress={logar}
+            >
+                <Text style={styles.buttonText}>Entrar</Text>
+            </TouchableOpacity>
+
         </KeyboardAvoidingView>
+
     )
 }
 
@@ -92,7 +88,7 @@ const styles = StyleSheet.create({
         fontSize: 24,
         fontWeight: "bold",
         color: "#1c1c1e",
-        marginBottom: 30 
+        marginBottom: 30
     },
     label: {
         alignSelf: "flex-start",
