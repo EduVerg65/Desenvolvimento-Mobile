699b29ba6028f9708ca9f15a171498efdffa2d7b
mangamadura <edu.vergentino.malaquias@gmail.com>
2026-04-08 22:31:48 -0300

    atualização no login

--- app/login.tsx
+++ app/login.tsx
@@ -3,6 +3,8 @@ import { useRouter } from "expo-router";
 import { useState } from "react";
 import { KeyboardAvoidingView, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View, Image } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { COLORS } from "../theme/colors";
+import { FONTS } from "../theme/fonts";
 
 export default function LoginScreen() {
     const router = useRouter();
@@ -20,46 +22,53 @@ export default function LoginScreen() {
         <SafeAreaView style={styles.container}>
             <KeyboardAvoidingView
                 behavior={Platform.OS === "ios" ? 'padding' : 'height'}
-                style={styles.content} 
+                style={styles.content}
             >
+
+                {/* LOGO (fica fora do card) */}
                 <View style={styles.innerContainer}>
                     <Image source={require('../img/logo.png')} style={styles.logo} />
                     <Text style={styles.title}>Acesse sua conta</Text>
                 </View>
 
-                <Text style={styles.label}>E-mail</Text>
-                <TextInput
-                    style={styles.input}
-                    placeholder="email@example.com"
-                    keyboardType="email-address"
-                    autoCapitalize="none"
-                />
+                {/* CARD branco */}
+                <View style={styles.card}>
 
-                <Text style={styles.label}>Senha</Text>
-                <View style={styles.passwordContainer}>
+                    <Text style={styles.label}>E-mail</Text>
                     <TextInput
-                        style={styles.passwordInput}
-                        placeholder="*********"
-                        secureTextEntry={secureText}
+                        style={styles.input}
+                        placeholder="email@example.com"
+                        keyboardType="email-address"
+                        autoCapitalize="none"
                     />
-                    <TouchableOpacity
-                        onPress={trocarEstadoSenha}
-                        style={styles.iconContainer}
-                    >
-                        <Ionicons
-                            name={secureText ? "eye-off-outline" : "eye-outline"}
-                            size={20}
-                            color={"#8e8e93"}
-                        />
-                    </TouchableOpacity>
-                </View>
 
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={logar}
-                >
-                    <Text style={styles.buttonText}>Entrar</Text>
-                </TouchableOpacity>
+                    <Text style={styles.label}>Senha</Text>
+                    <View style={styles.passwordContainer}>
+                        <TextInput
+                            style={styles.passwordInput}
+                            placeholder="*********"
+                            secureTextEntry={secureText}
+                        />
+                        <TouchableOpacity
+                            onPress={trocarEstadoSenha}
+                            style={styles.iconContainer}
+                        >
+                            <Ionicons
+                                name={secureText ? "eye-off-outline" : "eye-outline"}
+                                size={20}
+                                color={COLORS.textLight}
+                            />
+                        </TouchableOpacity>
+                    </View>
+
+                    <TouchableOpacity
+                        style={styles.button}
+                        onPress={logar}
+                    >
+                        <Text style={styles.buttonText}>Entrar</Text>
+                    </TouchableOpacity>
+
+                </View>
 
             </KeyboardAvoidingView>
         </SafeAreaView>
@@ -69,11 +78,11 @@ export default function LoginScreen() {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        backgroundColor: "#FFF",
+        backgroundColor: COLORS.primary, // verde Brasil
     },
     content: {
         flex: 1,
-        paddingHorizontal: 30, 
+        paddingHorizontal: 30,
     },
     innerContainer: {
         alignItems: "center",
@@ -86,27 +95,22 @@ const styles = StyleSheet.create({
         resizeMode: 'contain',
     },
     title: {
-        fontSize: 24,
-        fontWeight: "bold",
-        color: "#1c1c1e",
+        ...FONTS.title,
+        color: COLORS.accent, // azul Brasil 🇧🇷
         marginTop: 10,
     },
     label: {
-        fontSize: 14,
-        fontWeight: '600',
-        color: '#8e8e93',
+        ...FONTS.text,
+        color: COLORS.textLight,
         marginBottom: 5,
     },
     input: {
         width: "100%",
         height: 50,
-        borderWidth: 1,
-        borderColor: "#e5e5ea",
         borderRadius: 12,
         paddingHorizontal: 15,
         fontSize: 16,
-        color: "#1c1c1e",
-        backgroundColor: "#fbfbfd",
+        backgroundColor: "#F3F4F6",
         marginBottom: 15
     },
     passwordContainer: {
@@ -116,7 +120,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         borderColor: "#e5e5ea",
         borderRadius: 12,
-        backgroundColor: "#fbfbfd",
+        backgroundColor: "#F3F4F6",
         marginBottom: 20,
         alignItems: 'center'
     },
@@ -125,7 +129,7 @@ const styles = StyleSheet.create({
         height: '100%',
         paddingHorizontal: 15,
         fontSize: 16,
-        color: "#1c1c1e"
+        color: COLORS.text
     },
     iconContainer: {
         justifyContent: "center",
@@ -137,12 +141,20 @@ const styles = StyleSheet.create({
         borderRadius: 12,
         justifyContent: 'center',
         alignItems: 'center',
-        backgroundColor: "#fbcc05",
+        backgroundColor: COLORS.secondary, // amarelo 🇧🇷
         marginTop: 10
     },
     buttonText: {
         fontSize: 16,
         fontWeight: 'bold',
         color: "#FFF"
-    }
+    },
+    card: {
+        backgroundColor: "#fff",
+        borderTopLeftRadius: 30,
+        borderTopRightRadius: 30,
+        padding: 25,
+        flex: 1,
+        marginTop: 50
+    },
 });
